644c351de5eb382d5c3711b32a468a11fd463cce
LordOf20th <lordof20th@hotmail.fr>
2019-02-07 00:04:38 +0100

    Édition de la classe Arbre() et respect des conventions

--- classes.py
+++ classes.py
@@ -9,12 +9,23 @@ class Arbre:
     """ Classe définissant un arbre, caractérisé par :
     - Son état : Sain ou En feu (par défaut à sa création l'arbre est considéré Sain)
     - Sa position en x et y """
-    def __init__(self, x, y):
-        self.x=x
-        self.y=y
-        self.brule=False
-    def enflammer(self):
-        """ Méthode pour enflammer l'arbre"""
-        
-        if not self.brule:
-            self.brule = True
+    def __init__(self):
+        self._etat=0
+    def _get_etat(self):
+        """ Méthode pour lire l'état de l'arbre"""
+        return self._etat
+    def _set_etat(self, nouvel_etat):
+        """ Mutateur de l'attribut état """
+        if nouvel_etat == 0 or nouvel_etat == 1 or nouvel_etat == -1:
+            self._etat=nouvel_etat
+        else: 
+            raise ValueError("Valeur d'état invalide, seules 0, 1 et -1 sont valides")
+    etat=property(_get_etat, _set_etat)
+    def __repr__(self):
+        if self._etat == 0 :
+            return "Sain"
+        elif self._etat == 1:
+            return "En feu"
+        elif self._etat == -1:
+            return "Mort"
+            
