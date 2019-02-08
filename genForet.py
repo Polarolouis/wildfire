7de14e92b5244fffe59baf3129367b435a96693f
LordOf20th <lordof20th@hotmail.fr>
2019-02-08 17:29:06 +0100

    Update genForet.py

--- genForet.py
+++ genForet.py
@@ -6,10 +6,10 @@ Created on Fri Feb  1 11:30:02 2019
 """
 import numpy as np
 import random
-import tkinter as tk
+#import tkinter as tk
 
 #Fonctions agissant sur la matrice
-def genForet(n,p=0.25):
+def genForet(n,p):
     foret=np.full(shape=(n,n), fill_value=0)
     for i in range(0,foret.shape[0]):
         for j in range(0,foret.shape[1]):
@@ -17,18 +17,23 @@ def genForet(n,p=0.25):
                 foret[i,j]=1 
     return foret
 
-def enflammer( n, p=0.25,x=-1,y=-1): #Fonction mettant le feu aléatoirement à une case
-    foret = genForet(n,p)
+def enflammer(foret,x,y): #Fonction mettant le feu aléatoirement à une case
     if x==-1:
-        x=random.randint(0, foret.shape[0])
+        x=random.randint(0, foret.shape[0]-1)
     if y==-1:
-        y=random.randint(0, foret.shape[1])
+        y=random.randint(0, foret.shape[1]-1)
     foret[x,y]=-1
     return foret
     
-#def propagation(n, *p, *x, *y):
+def propagation(n, p=0.5, x=-1, y=-1):
+    F=[]
+    foret = genForet(n, p)
+    F.append(foret)
+    F.append(enflammer(foret,x,y))
+    return F #Return temporaire
     
-def affichage(foret):
+    
+""" def affichage(foret):
     
     fenetre = tk.Tk() #Crée la fenêtre que l'on va modifier
     
@@ -46,4 +51,4 @@ def affichage(foret):
     canvas.pack()
     bouton = tk.Button(fenetre, text="Quitter", command=fenetre.quit)
     bouton.pack()
-    fenetre.mainloop() #affiche la fenêtre
+    fenetre.mainloop() #affiche la fenêtre """ 
